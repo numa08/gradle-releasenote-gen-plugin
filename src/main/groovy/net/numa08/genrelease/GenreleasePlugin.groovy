301122d1c3a07b14172d4a734a7725a6d9c88467
numa08 <n511287@gmail.com>
2015-07-28 22:21:25 +0900

    [WIP]applyでTaskを登録すれば良いっぽい

--- src/main/groovy/net/numa08/genrelease/GenreleasePlugin.groovy
+++ src/main/groovy/net/numa08/genrelease/GenreleasePlugin.groovy
@@ -1,11 +1,35 @@
 package net.numa08.genrelease
 
+import net.numa08.genrelease.git.GitLogParser
+import net.numa08.genrelease.markdown.MarkdownConverter
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.Task
 
 class GenreleasePlugin implements Plugin<Project> {
+
+    public static final String TASK_NAME = "genReleaseNote"
+
     @Override
     void apply(Project project) {
-        // TODO
+        project.extensions.create(TASK_NAME, GenreleasePluginExtension)
+        final GenreleasePluginExtension extension = project.extensions.genReleaseNote
+        final Task createdTask = project.task(TASK_NAME) << { Task task ->
+            final Configuration configuration = new Configuration() {
+                @Override
+                String version() {
+                    return extension.version()
+                }
+            }
+
+            var parser = new GitLogParser(configuration)
+            var notes = parser.parse(extension.source())
+            if (notes._1() != null) {
+                var markdown = new MarkdownConverter().convert(notes._1())
+                println(markdown)
+            }
+        }
+
+        createdTask.description = "Create release note from your configuration"
     }
 }
---
+++ src/main/scala/net/numa08/genrelease/GenreleasePluginExtension.scala
@@ -0,0 +1,9 @@
+package net.numa08.genrelease
+
+class GenreleasePluginExtension {
+
+  var source: String = ""
+
+  var version: String = ""
+
+}
